45d2005af05c920ed3e23dc5d3e42914b915e805
rochadevcv <rochadevcv@gmail.com>
2025-03-04 07:06:06 -0300

    entidade usuario

---
+++ src/main/java/com/rocha/user/domain/entity/User.java
@@ -0,0 +1,34 @@
+package com.rocha.user.domain.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "TB_USER")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	private String name;
+	private String email;
+
+}
